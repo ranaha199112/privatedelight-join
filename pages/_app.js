225e0f5a1475ebd159035a7a36ce91b55dbec046
Zayn Yusuf <yusue786@gmail.com>
2024-05-08 14:32:58 +0600

    change meta name

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Home | megapersonals.com</title>
+        <title>Home | Private Delight</title>
       </Head>
 
       <ToastContainer />
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Home() {
-  return <div>megapersonals</div>;
+  return <div>Private Delight</div>;
 }
